714ef53b55186d3352d23e7890e33723d68571ee
jona <jonathan@gmail.com>
2020-06-28 16:35:16 -0300

    cambios

--- funcionesVACIAS.py
+++ funcionesVACIAS.py
@@ -3,32 +3,109 @@ from principal import *
 import math
 import random
 
+def lectura(nomArch):
+    nomArch=nomArch + ".txt"
+    archivo=open(nomArch,"r", encoding='utf-8')
+    salida=[]
+    while(True):
+        linea=archivo.readline().rstrip("\n")
+        if not linea:
+            break
+        else:
+            salida.append(linea.upper())
+    archivo.close()
+    return salida
+
+def lectura2(nomArch):
+    nomArch=nomArch + "-v2.txt"
+    archivo=open(nomArch,"r", encoding='utf-8')
+    salida=[]
+    i=0
+    while(True):
+        linea=archivo.readline()
+        if not linea:
+            break
+        else:
+            salida.append([])
+            salida[i].append(buscaPalabra(linea).upper())
+            if buscaPuntaje(linea).upper()== "":
+                salida[i].append(puntajeXLongitud(buscaPalabra(linea).upper()))
+            else:
+                salida[i].append(buscaPuntaje(linea).upper())
+            i+=1
+    archivo.close()
+    return salida
+
+def puntajeXLongitud(palabra):
+    if (len(palabra) > 7):
+        return 3;
+    elif (len(palabra) > 5):
+        return 2;
+    else:
+        return 1;
+    return 0;
+
+def buscaPalabra(linea):
+    cadena=""
+    for letra in linea:
+        if letra == "#" or letra=="\n":
+            break
+        else:
+            cadena+=letra
+    return cadena
+
+def buscaPuntaje(linea):
+    cadena=""
+    flag=0
+    cadena=""
+    for letra in linea:
+        if letra == "#":
+            flag=1
+        elif flag==1 and letra != "\n":
+            cadena+=letra
+    return cadena
+
 def unaAlAzar(lista): #hay que desarrollar.
-  return random.choice(lista)
+    return random.choice(lista)
 
-def CategoriaCorrecta(palabraUsuario, palabrasDeCategoria):
-    return palabraUsuario in palabrasDeCategoria
+def esCorrecta(palabraUsuario, letra, item, items, listaDeTodo,jugadaCompu):
+    palabraUsuario=palabraUsuario.upper()
+    letra=letra.upper()
+    aux=[]
+    puntaje=[]
+    for i in range(0, len(listaDeTodo[item])):
+        aux.append(listaDeTodo[item][i][0])
+        puntaje.append(listaDeTodo[item][i][1])
 
-def letraCorrecta(letra, palabraUsuario):
-    return letra == palabraUsuario[0]
-
-def esPalabraCorrecta(palabraUsuario, letra, listaDeTodo):
-    return CategoriaCorrecta(palabraUsuario, listaDeTodo) and letraCorrecta(letra, palabraUsuario)
-
-def esCorrecta(palabraUsuario, letra, listaDeTodo):
-    return 10 if esPalabraCorrecta(palabraUsuario, letra, listaDeTodo) else 0
-
+    if palabraUsuario != "" and palabraUsuario[0:1] == letra and palabraUsuario in aux:
+        n=puntaje[aux.index(palabraUsuario)]
+##aca se puede usar un moltiplicador o un +10 por ejemplo
+        multiplicador=1
+        if palabraUsuario == jugadaCompu[item]:
+            multiplicador=2
+        if n=="3":
+            return 15*multiplicador
+        elif n=="2":
+            return 10*multiplicador
+        else:
+            return 5*multiplicador
+    return 0
 
 def juegaCompu(letraAzar, listaDeTodo):
-    listaAux= []
-    listaCompu=[]
-    for i in range (0, len(listaDeTodo)):
-        del listaAux[:]
-        for palabra in listaDeTodo[i]:
-            if letraAzar == palabra[0]:
-                listaAux.append(palabra)
+    letraAzar=letraAzar.upper()
+    salida=[]
+    listaAux=[]
+    aux=[]
+    for i in range(0,len(listaDeTodo)):
+        aux.clear()
+        listaAux.clear()
+        for j in range(0, len(listaDeTodo[i])):
+            aux.append(listaDeTodo[i][j][0])
+        for parabra in aux:
+            if parabra[0:1] == letraAzar:
+                listaAux.append(parabra)
         if not listaAux:
-            listaAux.append("Palabra no encontada")
-        listaCompu.append(random.choice(listaAux))
-    return listaCompu
-
+            salida.append("sinPalabrasEnBase")
+        else:
+            salida.append(random.choice(listaAux))
+    return salida
--- principal.py
+++ principal.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
+#agregar seleccion o alta de un usuario
+#ranking de puntos
+#modos de juego
+#punto por dificultad de palabra
+#mas listas de cosas
+#que se retroalimente, es decir q al finalizar el juego te muestre una pantalla para confirmar el agregado de palabras
+#mas tardas, menos puntos. tipo un "jugas por"
+#
 
 import math, os, random, sys
-
 import pygame
+import time
+import datetime
 from pygame.locals import *
 
 from configuracion import *
 from funcionesVACIAS import *
 from extras import *
 
-
 def main():
     # centrar la ventana y despues inicializar pygame
     os.environ["SDL_VIDEO_CENTERED"] = "1"
     pygame.init()
-    # pygame.mixer.init()
 
     # preparar la ventana
     pygame.display.set_caption("TutiFrutiUNGS")
     screen = pygame.display.set_mode((ANCHO, ALTO))
-
-    #fondo del juego
-
 
     # Tiempo total del juego
     gameClock = pygame.time.Clock()
@@ -29,25 +33,27 @@ def main():
     fps = FPS_INICIAL
 
     puntos = 0
-    candidata = ""
 
-##    abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    abc=["a","v","c",]
+    abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    ##items=["colores","paises","animales","nombres"]
     items=["colores","paises","animales"]
-##    colores= lectura("colores")
-##    paises= lectura("paises")
-##    animales= lectura("animales")
+    ##colores= lectura("colores")
+    ##paises= lectura("paises")
+    ##animales= lectura("animales")
+    ##nombres= lectura("nombres")
 
-    colores=["rojo", "ruby","azul","a","amarillo","negro", "n","blanco", "b","celeste","c","verde","v","rosa", "r"]
-    paises=["argentina","uruguay","brasil","cuba","venezuela"]
-    animales=["mono","jirafa","gato","perro","jabali","elefante","pez","cocodrilo","rinoceronte","caballo"]
-    listaDeTodo=[colores,paises,animales]
-    print(colores)
+    paises2=lectura2("paises")
+    animales2=lectura2("animales")
+    colores2=lectura2("colores")
+    listaDeTodo=[colores2,paises2,animales2]
     letraAzar = unaAlAzar(abc)
     palabraUsuario=""
     eleccionUsuario=[]
-    eleccionCompu=[]
+    eleccionCompu=juegaCompu(letraAzar, listaDeTodo)
     i=0
+    auxTime=0
+    now = datetime.datetime.now()
+##    parcial=datetime.datetime.now()
     while i < len(items):
         # 1 frame cada 1/fps segundos
         gameClock.tick(fps)
@@ -66,36 +72,33 @@ def main():
                     if e.key == K_BACKSPACE:
                         palabraUsuario = palabraUsuario[0:len(palabraUsuario)-1]
                     if e.key == K_RETURN:
-                        eleccionUsuario.append(palabraUsuario)
-                        #chequea si es correcta y suma o resta puntos
-                        sumar=esCorrecta(palabraUsuario, letraAzar, listaDeTodo[i])
+                        now=datetime.datetime.now()
+                        eleccionUsuario.append(palabraUsuario.upper())
+                        sumar=esCorrecta(palabraUsuario, letraAzar, i, items, listaDeTodo,eleccionCompu)
                         puntos+=sumar
                         palabraUsuario=""
                         i=i+1
-
-        segundos = pygame.time.get_ticks() / 1000
+        segundostot = pygame.time.get_ticks() / 1000
+        segundos=now.second - datetime.datetime.now().second + 15
+        if segundos ==0:
+            now=datetime.datetime.now()
+            eleccionUsuario.append("")
+            i=i+1
 
         # limpiar pantalla anterior
-        fondo = pygame.image.load("fondoJuego.jpg")
-        screen.blit(fondo,[-150,-22])
+        screen.fill(COLOR_FONDO)
         if i<len(items):
             dibujar(screen, letraAzar, items[i], palabraUsuario, puntos, segundos)
         else:
-            eleccionCompu=juegaCompu(letraAzar, listaDeTodo)
-            dibujarSalida(screen, letraAzar, items, eleccionUsuario, eleccionCompu, puntos, segundos)
+            dibujarSalida(screen, letraAzar, items, eleccionUsuario, eleccionCompu, puntos, segundostot)
         pygame.display.flip()
 
-
-
     while True:
-
-
         for e in pygame.event.get():
-
+            le=e.type
             if e.type == QUIT:
                 pygame.quit()
                 return
-
 
 if __name__ == "__main__":
     main()
